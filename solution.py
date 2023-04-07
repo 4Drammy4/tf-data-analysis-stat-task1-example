a83206fa0c50e849af91798bbc3490d0fa0212f1
4Drammy4 <90786830+4Drammy4@users.noreply.github.com>
2023-04-07 21:30:49 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,15 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 416934694 # Ваш chat ID, не меняйте название переменной
 
+import numpy as np
+from scipy.stats import expon
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    t = 10
+    n = len(x)
+    mu=-23
+    sigma = np.exp(1)
+    v = expon.rvs(mu,sigma,n)
+    a = v/t
+    return 1/a.mean()
