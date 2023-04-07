67442226fff355b660445b9ebe4d1cd336add2c8
4Drammy4 <90786830+4Drammy4@users.noreply.github.com>
2023-04-07 21:51:44 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,16 +1,11 @@
-import pandas as pd
-import numpy as np
-
-
-chat_id = 416934694 # Ваш chat ID, не меняйте название переменной
-
 import numpy as np
 from scipy.stats import expon
 def solution(x: np.array) -> float:
     t = 10
     n = len(x)
-    mu=-23
-    sigma = np.exp(1)
-    v = expon.rvs(mu,sigma,n)
-    a = v/t
-    return 1/a.mean()
+    shift = -23
+    lambda_param = 1
+    import numpy as np
+    v = shift + np.random.exponential(1/lambda_param, size=n)
+    lambda_hat = 1 / np.mean(v - shift)
+    return lambda_hat
